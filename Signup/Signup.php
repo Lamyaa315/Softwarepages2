b06c18807f64cfee9646cdc3687706bef69f5268
NorahAlfaheed <noraha778@gmail.com>
2025-04-10 21:39:50 +0300

    Update Signup.php

--- Signup/Signup.php
+++ Signup/Signup.php
@@ -6,12 +6,8 @@ $username = "root";
 $password = "root";
 $database = "ruwaa";
 
-<<<<<<< Updated upstream
-$conn = mysqli_connect($servername, $username, $password, $database);
-=======
 // الاتصال
 $conn = mysqli_connect($servername, $username, $password, $database,8889);
->>>>>>> Stashed changes
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
